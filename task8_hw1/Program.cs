80dc79aac3dc902d5366821d674e7463ef44d1ff
bakaraulov <b.karaulov@ya.ru>
2023-05-21 06:23:20 +0300

    adding hw1 task8

---
+++ task8_hw1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+Console.WriteLine("Введите число больше нуля: ");
+int number1 = int.Parse(Console.ReadLine ()!);
+int number2 = number1 * -1;
+int temp = 2;
+
+if ( number1 > 2)
+    {
+    while (temp <= number1)
+        {
+        Console.Write(temp + " ");
+        temp += 2;
+        }
+    }
+else
+    {
+    Console.Write("Вы ввели слишком маленькое начение");
+    }
