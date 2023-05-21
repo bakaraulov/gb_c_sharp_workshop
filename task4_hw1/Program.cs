9abafedcd9472dea602009d6bcdac8bdc0ee8427
bakaraulov <b.karaulov@ya.ru>
2023-05-21 05:55:54 +0300

    adding hw1 task4

---
+++ task4_hw1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел
+Console.WriteLine("Введите число1: ");
+int number1 = int.Parse(Console.ReadLine ()!);
+Console.WriteLine("Введите число2: ");
+int number2 = int.Parse(Console.ReadLine ()!);
+Console.WriteLine("Введите число3: ");
+int number3 = int.Parse(Console.ReadLine ()!);
+int max = number1;
+
+if (number2 >= max)
+    {
+    max = number2;
+    }
+if (number3 >= max)
+    {
+    max = number3;
+    }
+Console.WriteLine("Максимальное введенное число: " + max);
