9ccdc33c0344fa59aefbcfb5826c3f1db32d1073
bakaraulov <b.karaulov@ya.ru>
2023-06-01 17:25:44 +0300

    task29 adding manual arrey

--- task29_hw4/Program.cs
+++ task29_hw4/Program.cs
@@ -14,6 +14,12 @@ int Prompt (string msg)
     }
     return val;
 }
+string Prompt2 (string msg)
+{
+    Console.Write(msg);
+    string value = Console.ReadLine();
+    return value;
+}
 
 int[] CreateRandomMassive(int val, int min, int max)
 {
@@ -27,6 +33,19 @@ int[] CreateRandomMassive(int val, int min, int max)
     return array;
 }
 
+int[] CreatemManualMassive(int val)
+{
+    // int [] array = new int [8];
+    int [] array = new int [val];
+    for (int i=0; i<array.Length; i++)
+    {
+        int item = i+1;
+        array[i] = Prompt ($"Введите значение {item} элемента: ");
+        // Console.Write (array[i]+" ");
+    }
+    return array;
+}
+
 void PrintMassive(int[] array)
 {
     for (int i=0; i<array.Length; i++)
@@ -34,16 +53,37 @@ void PrintMassive(int[] array)
         Console.Write (array[i]+" ");
     }
 }
+string q1 = Prompt2("Вы хотите создать массив в ручную? (Y-да, N-нет): ");
 
-int value = Prompt("Введите длинну массива: ");
-int min = Prompt("Введите значение минимального эл-та массива: ");
-int max = Prompt("Введите значение максимального эл-та массива: ");
-Console.WriteLine ($"{value}, {min}, {max}");
-if (value>0)
+if (q1.ToLower() == "n")
 {
-    PrintMassive (CreateRandomMassive(value, min, max));
+    int value = Prompt("Введите длинну массива: ");
+    int min = Prompt("Введите значение минимального эл-та массива: ");
+    int max = Prompt("Введите значение максимального эл-та массива: ");
+    Console.WriteLine ($"{value}, {min}, {max}");
+    if (value>0)
+    {
+        PrintMassive (CreateRandomMassive(value, min, max));
+    }
+    else
+    {
+        Console.WriteLine ("Задано неверное число элементов. Нельзя создать массив");
+    }
+}
+else if (q1.ToLower() == "y")
+{
+    int value = Prompt("Введите длинну массива: ");
+    Console.WriteLine ($"{value}");
+    if (value>0)
+    {
+        PrintMassive (CreatemManualMassive(value));
+    }
+    else
+    {
+        Console.WriteLine ("Задано неверное число элементов. Нельзя создать массив");
+    }
 }
 else
 {
-    Console.WriteLine ("Задано неверное число элементов. Нельзя создать массив");
+    Console.WriteLine ("Введен неверный символ");
 }
