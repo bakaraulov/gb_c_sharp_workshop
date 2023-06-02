5c91d1772ccccdbf21d6781ed0e59d823849dd2f
bakaraulov <b.karaulov@ya.ru>
2023-06-02 07:20:35 +0300

    task25 added mins degree

--- task25_hw4/Program.cs
+++ task25_hw4/Program.cs
@@ -19,7 +19,19 @@ int Prompt (string msg)
 int Degree (int arg1, int arg2)
 {
     int degree = arg1;
-    for (int i = 0; i < Math.Abs(arg2); i++)
+    for (int i = 1; i < Math.Abs(arg2); i++)
+    {
+        degree = degree*arg1;
+        // Console.WriteLine(degree + " ");
+        // Console.WriteLine(i + " ");
+    }
+    return degree;
+}
+
+double Degree2 (int arg1, int arg2)
+{
+    double degree = arg1;
+    for (int i = 1; i < Math.Abs(arg2); i++)
     {
         degree = degree*arg1;
         // Console.WriteLine(degree + " ");
@@ -30,16 +42,24 @@ int Degree (int arg1, int arg2)
 
 int num1 = Prompt("Введите число1: ");
 int num2 = Prompt("Введите число2: ");
-int degree = 0;
+int degree1 = 0;
+double degree2 = 0;
+
+// Console.WriteLine($"{num1} {num2} {degree}");
 
 if (Math.Abs(num2) == 1)
 {
     Console.WriteLine($"Возведенное число равно: {num1}");
 }
-else if (num2>1 || num2<-1)
+else if (num2>0)
 {
-    degree = Degree(num1, num2);
-    Console.WriteLine($"c");
+    degree1 = Degree(num1, num2);
+    Console.WriteLine($"Возведенное число равно: {degree1}");
+}
+else if (num2<0)
+{
+    degree2 = 1/(Degree2(num1, Math.Abs(num2)));
+    Console.WriteLine($"Возведенное число равно: {degree2}");
 }
 else
 {
