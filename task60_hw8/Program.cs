a31a32d554481638bd3f7517f154e11e7843cf4e
bakaraulov <b.karaulov@ya.ru>
2023-06-25 16:28:30 +0300

    added hw8

--- task60_hw8/Program.cs
+++ task60_hw8/Program.cs
@@ -5,3 +5,102 @@
 // 27(0,0,1) 90(0,1,1)
 // 26(1,0,1) 55(1,1,1)
 
+int Prompt (string msg)
+{
+    Console.Write(msg);
+    string value = Console.ReadLine();
+    int val = 0;
+    while ((int.TryParse(value, out val)) != true)
+    {
+        Console.Write("Неверный ввод. Повторите: ");
+        value = Console.ReadLine();
+    }
+    Console.Clear();
+    return val;
+}
+
+void PrintMatrix (int[,,] matrix)
+{
+    for (int i = 0 ; i < matrix.GetLength(0) ; i ++)
+    {
+        for (int j = 0 ; j<matrix.GetLength(1) ; j ++)
+        {
+            for (int l = 0; l<matrix.GetLength(2); l++)
+            {
+                Console.WriteLine($" Элемент {matrix [i , j , l]} имеет индекс в массиве ({i},{j},{l}) ");
+            }
+        }
+    Console.WriteLine();
+    }
+}
+
+int[,,] CreateMRandoMatrix (int rows , int colums , int space, int from , int to)
+{
+    int [,,] matrix = new int[rows,colums,space];
+    to ++;
+    for (int i = 0 ; i < matrix.GetLength(0) ;i ++)
+    {
+        for (int j = 0 ; j<matrix.GetLength(1) ; j ++)
+        {
+            for (int l = 0; l<matrix.GetLength(2); l++)
+            {
+                matrix[i,j,l] = new Random().Next(from,to);
+            
+            }
+        }
+    }
+    return matrix;
+}
+
+void ExamRandomMatrix(int[,,] matrix3D)
+{
+    int[] temp = new int[matrix3D.Length];
+    int tempNumber;
+    for (int i = 0; i < temp.Length; i++)
+    {
+        temp[i] = new Random().Next(10, 100);
+        tempNumber = temp[i];
+        if (i >= 1)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                while (temp[i] == temp[j])
+                {
+                    temp[i] = new Random().Next(10, 100);
+                    j = 0;
+                    tempNumber = temp[i];
+                }
+            }
+        }
+    }
+    int count = 0;
+    for (int i = 0; i < matrix3D.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix3D.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix3D.GetLength(2); k++)
+            {
+                matrix3D[i,j,k] = temp[count];
+                count ++;
+            }
+        }
+    }
+}
+
+int row = Prompt("Ведите количество строк массива: ");
+int col = Prompt("Введите количество столбцов массива: ");
+int space = Prompt("Введите конечное элементов в пространстве массива: ");
+int from = Prompt("Введите начальное значение элементов массива: ");
+int to = Prompt("Введите конечное значение элементов массива: ");
+
+
+Console.WriteLine($"Вы ввели: {row} , {col} , {space}, {from} , {to}");
+Console.WriteLine();
+
+int[,,] matrix = CreateMRandoMatrix(row,col,space,from,to);
+// PrintMatrix(matrix);
+Console.WriteLine();
+
+ExamRandomMatrix(matrix);
+PrintMatrix(matrix);
+Console.WriteLine();
---
+++ task63_ws9/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 63: Задайте значение N. Напишите программу, которая выведет все натуральные числа 
+// в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+// Console.Clear();
+
+// int Prompt (string msg)
+// {
+//     Console.Write(msg);
+//     string value = Console.ReadLine();
+//     int val = 0;
+//     while ((int.TryParse(value, out val)) != true)
+//     {
+//         Console.Write("Неверный ввод. Повторите: ");
+//         value = Console.ReadLine();
+//     }
+//     Console.Clear();
+//     return val;
+// }
+
+// int number = Prompt("Введиче число: ");
+// int i = 1;
+
+
+// void NaturalNumber(int number,int count)
+// {    
+//     if (count<=number)
+//     {
+//         Console.Write($"{count} ");
+//         NaturalNumber(number,count+1);
+//     }
+// }
+
+// NaturalNumber(number,i);
+
+Console.Clear();
+
+int number = Prompt("Введите число: ");
+Console.WriteLine(PrintNumber(number));
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine()!);
+    return number;
+}
+
+string PrintNumber(int number)
+{
+    if (number == 1) return "1";
+
+    // return $"{PrintNumber(number - 1)} {number}";    // меньшего> к большему
+    return $"{number} {PrintNumber(number - 1)} ";
+}
